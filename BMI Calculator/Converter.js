6b7842a44fd9e15c6b92efdadeab3e442512a242
RENNNNNNNNNNN <128695507+ODALDMAWPKJD@users.noreply.github.com>
2024-09-18 21:16:24 +0800

    Add files via upload

---
+++ BMI Calculator/Converter.js
@@ -0,0 +1,40 @@
+const form = document.getElementById("bmiForm");
+const result = document.getElementById("bmiValue")
+const category = document.getElementById("bmiCategory")
+
+form.addEventListener("submit", (event) =>{
+event.preventDefault()
+DisplayValue()
+})
+
+function DisplayValue(){
+    const height = document.getElementById("height").value;
+    const weight = document.getElementById("weight").value;
+
+    let heightConvertMeters = height / 100
+
+    let bmiResult = weight / ( heightConvertMeters*heightConvertMeters)
+
+    const input = document.querySelectorAll("input");
+
+    input.forEach((element) =>{
+        element.value = "";
+    })
+    result.textContent = bmiResult.toFixed(2);
+
+    if(bmiResult < 18.5){
+        category.textContent = "Underweight";
+        alert("KAMATYONON NAKA GO")
+    }
+    else if(bmiResult >= 18.5 || bmiResult<= 24.9){
+        category.textContent = "Normal weight"
+    }
+    else if(bmiResult >= 25|| bmiResult <= 29.9){
+        category.textContent = "Overweight"
+        alert("KAMATYONON NAKA GO")
+    }
+    else if(bmiResult >= 30){
+        category.textContent = "Obesity"
+        alert("PATAY NA ANG GAGO")
+    }
+}
